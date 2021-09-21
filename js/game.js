20490c21e3f790348cba0afe23c0610d7f822e87
Jan <hiasguy@hotmail.com>
2021-09-21 11:20:42 +0200

    added classes.js files

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
-
+let world = new World();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    character.src = '../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
+    console.log('My character is', world.character);
 
-    ctx.drawImage(character, 20, 20, 50, 150);
 }
