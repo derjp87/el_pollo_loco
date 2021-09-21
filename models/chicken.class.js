94923fcec28b60abe67b97609f4a2ce7f3e563ab
Jan <hiasguy@hotmail.com>
2021-09-21 14:41:48 +0200

    added clouds

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,6 +2,8 @@ class Chicken extends MoveableObject {
 
     constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
+    
+        this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700
     }
 
 }
---
+++ models/cloud.class.js
@@ -0,0 +1,12 @@
+class Cloud extends MoveableObject {
+    y = 20;
+    width = 500;
+    height = 250;
+
+    constructor() {
+        super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
+    
+        this.x = Math.random() * 500; //Zahl zwischen 200 und 700
+
+    }
+}
