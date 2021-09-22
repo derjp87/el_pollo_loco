07834966f46975497643738c60c23f13a1b2899a
Jan <hiasguy@hotmail.com>
2021-09-22 22:07:15 +0200

    merge conflict cleared

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,15 +1,10 @@
 class BackgroundObject extends MoveableObject {
 
-<<<<<<< HEAD
-    constructor(imagePath) {
-        super().loadImage(imagePath);
-=======
     width = 720;
     height = 400;
     constructor(imagePath, x, y) {
         super().loadImage(imagePath);
         this.x = x;
         this.y = y;
->>>>>>> 226f97eae2b8239079b80add9f6001b823f03d4a
     }
 }
