8f6322f5a7aeeb3bcd1dbf946e3bb52d17e7e191
Jan <hiasguy@hotmail.com>
2021-09-22 23:24:18 +0200

    added animation to objects

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,6 +7,11 @@ class Cloud extends MoveableObject {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
     
         this.x = Math.random() * 500; //Zahl zwischen 200 und 700
-
+        this.animate();
     }
+
+    animate() {
+        this.moveLeft();
+    }
+
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,13 +4,34 @@ class MoveableObject {
         img;
         height = 150;
         width = 100;
+        imageCache = {};
+        currentImage = 0;
+        speed = 0.15;
 
         loadImage(path) {
             this.img = new Image();
             this.img.src = path;
         }
 
+        /**
+         * 
+         * @param {Array} arr ['img/image1.png', 'img/image2.png', ...]
+         */
+        loadImages(arr){
+            arr.forEach((path) => {
+                let img = new Image();
+                img.src = path;
+                this.imageCache[path] = img;
+            });
+        }
+
         moveRight() {
             console.log('Moving right');
+        }
+        
+        moveLeft() {
+            setInterval(() => {
+                this.x -= this.speed;
+            }, 1000 / 60);
         }
 }
