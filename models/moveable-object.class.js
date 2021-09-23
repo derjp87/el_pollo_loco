f954e27c93d975d44feec4d6b92d0e07937abf95
Jan <hiasguy@hotmail.com>
2021-09-23 22:10:33 +0200

    start adding statusbar

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,11 +1,4 @@
-class MoveableObject {
-        x = 120;
-        y = 280;
-        img;
-        height = 150;
-        width = 100;
-        imageCache = {};
-        currentImage = 0;
+class MoveableObject extends DrawableObject {
         speed = 0.15;
         otherDirection = false;
         speedY = 0;
@@ -25,25 +18,8 @@ class MoveableObject {
             return this.y < 145;
         }
 
-        loadImage(path) {
-            this.img = new Image();
-            this.img.src = path;
-        }
 
-        draw(ctx) {
-            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
 
-        drawFrame(ctx) {
-
-            if(this instanceof Character || this instanceof Chicken) {
-                ctx.beginPath();
-                ctx.lineWidth = '5';
-                ctx.strokeStyle = 'blue';
-                ctx.rect(this.x, this.y, this.width, this.height);
-                ctx.stroke();
-            }
-        }
 
         // character.isColliding(chicken);
         isColliding(mo) {
@@ -76,13 +52,7 @@ class MoveableObject {
          * 
          * @param {Array} arr ['img/image1.png', 'img/image2.png', ...]
          */
-        loadImages(arr){
-            arr.forEach((path) => {
-                let img = new Image();
-                img.src = path;
-                this.imageCache[path] = img;
-            });
-        }
+
 
         playAnimation(images) {
             let i = this.currentImage % images.length;
