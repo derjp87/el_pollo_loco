d0bb29ec1593387738d75997b6aa50b26b0dbb68
Jan <hiasguy@hotmail.com>
2021-09-24 11:41:26 +0200

    fixed error

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -6,7 +6,7 @@ class StatusBar extends DrawableObject {
         'img/7.Marcadores/Barra/Marcador vida/azul/40_.png',
         'img/7.Marcadores/Barra/Marcador vida/azul/60_.png',
         'img/7.Marcadores/Barra/Marcador vida/azul/80_.png',
-        'img/7.Marcadores/Barra/Marcador vida/azul/110_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/100_.png',
     ];
 
     percentage = 100;
